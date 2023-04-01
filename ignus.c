9fcdc9900cbb1720bfbd0af1ec0d3a0e06f90b17
danlee2002 <danlee@gatech.edu>
2023-04-01 15:28:21 -0400

    set flags

--- ignus.c
+++ ignus.c
@@ -12,6 +12,7 @@ void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios; //copies original structure
+    raw.c_iflag &= ~(IXON);
     raw.c_lflag &= ~(ECHO|ICANON); //turns off canonical mode
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
