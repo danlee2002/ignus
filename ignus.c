28afb7501d754d3cc29b4b9fc71dad3888f4545e
danlee2002 <danlee@gatech.edu>
2023-04-01 16:12:11 -0400

    macros

--- ignus.c
+++ ignus.c
@@ -1,8 +1,11 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#define CTRL_KEY(k) ((k) & 0x1f)
+//data
 struct termios orig_termios;
 
 //error handling 
@@ -12,11 +15,11 @@ void die(const char *s) {
 }
 //disables raw mode and restores default
 void disableRawMode() { //restores defaults 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 //method to prevent echoing of characters within terminal 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
     struct termios raw = orig_termios; //copies original structure
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //turns off misc flags
@@ -26,20 +29,21 @@ void enableRawMode() {
     raw.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG); //turns off echoing/canonical mode/disables signal
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcgetattr");
 }
+
 int main () {
     enableRawMode(); //turns on raw mode
     //program exits as soon as q is pressed, else reads input based on character
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
         printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   }
     return 0; 
 }
