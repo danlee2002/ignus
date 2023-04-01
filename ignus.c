3b45b8705f768c71d6a92f7cb60f8dabd8406d85
danlee2002 <danlee@gatech.edu>
2023-04-01 15:56:39 -0400

    added error handling

--- ignus.c
+++ ignus.c
@@ -4,6 +4,13 @@
 #include <termios.h>
 #include <unistd.h>
 struct termios orig_termios;
+
+//error handling 
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+//disables raw mode and restores default
 void disableRawMode() { //restores defaults 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
@@ -12,20 +19,27 @@ void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
     struct termios raw = orig_termios; //copies original structure
-    raw.c_iflag &= ~(IXON);
-    raw.c_lflag &= ~(ECHO|ICANON); //turns off canonical mode
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); //turns off misc flags
+    raw.c_iflag &= ~(ICRNL|IXON); //turns off start and output control
+    raw.c_cflag |= (CS8);
+    raw.c_oflag &= ~(OPOST); //turns off '\n' and '\r\n'
+    raw.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG); //turns off echoing/canonical mode/disables signal
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main () {
     enableRawMode(); //turns on raw mode
-    char c;
     //program exits as soon as q is pressed, else reads input based on character
-    while (read(STDIN_FILENO, &c,1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if (iscntrl(c)) {
-            printf("%d\n",c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d('%c')\n",c,c);
-        }
+        printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q') break;
+  }
     return 0; 
 }
